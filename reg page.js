9b46371b883653bb3b4b0d4f73cfbc881677796a
monish17 <monishkumarft@gmail.com>
2023-10-23 17:55:04 +0530

    Appointment booking App

--- reg page.js
+++ reg page.js
@@ -8,15 +8,12 @@ const msg=document.querySelector('.msg')
 myform.addEventListener('submit',onSubmit);
 function onSubmit(e){
     e.preventDefault();
-    //console.log(nameInput.value);
-    //console.log(schedule.value);
     if (nameInput.value===''||email.value===''||phonenumber.value==='' || schedule.value===''){
         msg.style.color='red';
         msg.innerHTML='please enter all the fields';
         setTimeout(() =>msg.remove(),3000);
     }
     else{
-       console.log('success');
        localStorage.setItem('name',nameInput.value);
        localStorage.setItem('email',email.value);
        localStorage.setItem('phonenumber',phonenumber.value);

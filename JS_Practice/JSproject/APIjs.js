913c7b723fdd4f0ad93df7edd9473cb46a100a23
monish17 <monishkumarft@gmail.com>
2023-11-18 00:31:19 +0530

    JS Project API

--- JS_Practice/JSproject/APIjs.js
+++ JS_Practice/JSproject/APIjs.js
@@ -1,14 +1,12 @@
 const myform=document.querySelector('#myform');
 const DishPrice=document.querySelector('#price');
 const Dishitem=document.querySelector('#dish');
-const table=document.querySelector('#table');
 const msg=document.querySelector('.msg')
+const ul=document.querySelector('#ul');
 myform.addEventListener('submit',onSubmit);
 function onSubmit(e){
     e.preventDefault();
-    const selectedTableId = table.value;
-    const selectedTable = document.getElementById(selectedTableId);
-    if (DishPrice.value===''||Dishitem.value===''||table.value===''){
+    if (DishPrice.value===''||Dishitem.value===''){
         msg.style.color='red';
         msg.innerHTML='please enter all the fields';
         setTimeout(() =>msg.remove(),3000);
@@ -18,17 +16,17 @@ function onSubmit(e){
         const del=document.createElement('input');
         del.type='button';
         del.value='Delete Order';
-        li.appendChild(document.createTextNode(`${DishPrice.value}-${table.value}-${Dishitem.value}`));
+        li.appendChild(document.createTextNode(`${DishPrice.value}-${Dishitem.value}`));
         const separator=document.createTextNode(' ');
         li.append(separator);
         li.appendChild(del);
-        selectedTable.appendChild(li);
+        ul.appendChild(li);
         const myobj={
             Dish_price:DishPrice.value,
-            Table:table.value,
             Dish_Item:Dishitem.value,
         }
-        axios.post("https://crudcrud.com/api/8132d5b330974d3eaa32a173a31b581f/userData",myobj)
+        updateTotalValue(DishPrice.value);
+        axios.post("https://crudcrud.com/api/d795cf47418d46288cef50a59ee50666/userData",myobj)
         .then((Response)=>{
             console.log(Response);
             console.log('uploaded to the server')
@@ -40,14 +38,13 @@ function onSubmit(e){
         del.addEventListener('click',deleteClick);
         DishPrice.value=''
         Dishitem.value=''
-        table.value=''
     }
 }
 function getOrderIdByTableNo(TableNo) {
     return new Promise((resolve, reject) => {
-        axios.get('https://crudcrud.com/api/8132d5b330974d3eaa32a173a31b581f/userData')
+        axios.get('https://crudcrud.com/api/d795cf47418d46288cef50a59ee50666/userData')
             .then((response) => {
-                const OrderData = response.data.find(entry => entry.Table === TableNo);
+                const OrderData = response.data.find(entry => entry.Dish_Item === TableNo);
                 // console.log('orderdata._id');
                 // console.log(OrderData._id);
                 resolve(OrderData ? OrderData._id : null);
@@ -61,7 +58,7 @@ function getOrderIdByTableNo(TableNo) {
 }
 function deleteOrderDetails(Order_id) {
     return new Promise((resolve, reject) => {
-        axios.delete(`https://crudcrud.com/api/8132d5b330974d3eaa32a173a31b581f/userData/${Order_id}`)
+        axios.delete(`https://crudcrud.com/api/d795cf47418d46288cef50a59ee50666/userData/${Order_id}`)
             .then((response) => {
                 console.log(response);
                 console.log('UserDetails deleted from server');
@@ -76,21 +73,20 @@ function deleteOrderDetails(Order_id) {
     });
 }
 function showOrderDetailsOnScreen(user) {
-    const selectedTableId = user.Table;
-    const selectedTable = document.getElementById(selectedTableId)
     const li = document.createElement('li');
     const del = document.createElement('input');
     del.type = 'button';
-    del.value = 'Delete Order';
-    li.appendChild(document.createTextNode(`${user.Dish_price}-${user.Table}-${user.Dish_Item}`));
+    del.value = 'Delete Product';
+    li.appendChild(document.createTextNode(`${user.Dish_price}-${user.Dish_Item}`));
     const separator=document.createTextNode(' ');
     li.append(separator);
     li.appendChild(del);
-    selectedTable.appendChild(li);    
+    ul.appendChild(li);
+    updateTotalValue(user.Dish_price);   
     del.addEventListener('click',deleteClick);
 }
 window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/8132d5b330974d3eaa32a173a31b581f/userData")
+    axios.get("https://crudcrud.com/api/d795cf47418d46288cef50a59ee50666/userData")
         .then((response)=>{
             console.log(response)
             for(var i=0;i<response.data.length;i++){
@@ -108,16 +104,18 @@ async function deleteClick(e) {
         const liText = li.textContent;
         //console.log(li.textContent);
         const parts = liText.split('-');
-        const Table = parts[1];
+        const price=parts[0].trim();
+        const Product = parts[1].trim();
         
         try {
-            const OrderId = await getOrderIdByTableNo(Table);
+            const OrderId = await getOrderIdByTableNo(Product);
 
             if (OrderId) {
                 const success = await deleteOrderDetails(OrderId);
 
                 if (success) {
                     li.remove();
+                    DeleteTotalValue(price);
                 } else {
                     console.log('Problem in deleteUserDetails function');
                 }
@@ -129,4 +127,16 @@ async function deleteClick(e) {
             console.log('Error in deleteClick function');
         }
     }
+}
+function updateTotalValue(value) {
+    const totalValueSpan = document.getElementById("totalValue");
+    let currentTotal = parseFloat(totalValueSpan.textContent);
+    const numericValue = parseFloat(value);
+    totalValueSpan.textContent = currentTotal + numericValue;
+}
+function DeleteTotalValue(value) {
+    const totalValueSpan = document.getElementById("totalValue");
+    let currentTotal = parseFloat(totalValueSpan.textContent);
+    const numericValue = parseFloat(value);
+    totalValueSpan.textContent = currentTotal - numericValue;
 }

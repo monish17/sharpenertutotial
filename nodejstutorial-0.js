247b7d7ad9f21a9bf65be53feed7857fae2aa476
monish17 <monishkumarft@gmail.com>
2023-12-16 21:49:34 +0530

    tut-1(arrow function and object)

--- nodejstutorial-0.js
+++ nodejstutorial-0.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const product=(a,b)=>a*b;
+console.log(product(2,5));
+const student={
+    name:'Rahul',
+    age:45,
+    profession:'teacher'
+}
+console.log(student);

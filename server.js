d716a4772eb9a17326264d5279e382242f9a09b3
monish17 <monishkumarft@gmail.com>
2023-12-20 12:27:34 +0530

    Based on different url request showing different message on webpage from server

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const http= require('http');
 const server = http.createServer((req,res)=>{
-    console.log('Monish kumar ');
+    if(req.url ==='/home'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>my first page</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html');
+        res.end();
+    }else if(req.url==='/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>my first page</title></head>');
+        res.write('<body><h1>Welcome to About us Page</h1></body>')
+        res.write('</html');
+        res.end();
+    }else if(req.url==='/node'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>my first page</title></head>');
+        res.write('<body><h1>Hello from my node js server</h1></body>')
+        res.write('</html');
+        res.end();
+    }
 });
 server.listen(4000);

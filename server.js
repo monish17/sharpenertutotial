3708ef0121b47278f83fae424441312ee0b96985
monish17 <monishkumarft@gmail.com>
2023-12-25 23:49:15 +0530

    Different routes and body parser methods from express js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 
 const express=require('express');
 
+const bodyparser=require('body-parser');
+
 const app=express();
-app.use((req,res,next)=>{
-    console.log('this is a middleware function');
-    next()
+app.use(bodyparser.urlencoded());
+app.use('/Add-products',(req,res,next)=>{
+    res.send('<form action="/products" method="POST"><input type="text" name="Title"><br><input type="number" name="size"><br><button type="submit">Add Products</button></form>');
 })
-app.use((req,res,next)=>{
-    console.log("another middleware");
-    res.send('<h1>hello everyone!</h1>');
+app.post('/products',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Product Added!</h1>');
 })
 app.listen(3000);

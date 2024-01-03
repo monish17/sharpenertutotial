861fd7c61215f6e1d989845ede814f3f2314d0a5
monish17 <monishkumarft@gmail.com>
2024-01-03 15:39:37 +0530

    models

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,22 @@
 const path=require('path');
 const root=require('../helper functions/path');
+const Product = require('../models/product');
+
 exports.getAddProducts=(req,res,next)=>{
     res.sendFile(path.join(root,'views','addProduct.html'));
 }
 
 exports.postAddProducts=(req,res,next)=>{
-    console.log(req.body);
+    const product=new Product(req.body.title);
+    product.save();
+    console.log(req.body.title);
     res.redirect('/');
 }
 
 exports.getProducts=(req,res,next)=>{
+    // Product.fetchAll(product=>{
+    //     //res.sendFile(path.join(root,'views','shop.html'))
+    //     console.log(product);
+    // });
     res.sendFile(path.join(root,'views','shop.html'))
-}
+};

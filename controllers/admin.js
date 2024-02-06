ba94f4a535f1b9fbd7cba0371dd4492227c17b54
monish17 <monishkumarft@gmail.com>
2024-02-06 22:47:42 +0530

    Sequelize

---
+++ controllers/admin.js
@@ -0,0 +1,80 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing:false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null,title, imageUrl, description, price);
+  product.save()
+  .then(()=>{
+    res.redirect('/');
+  })
+  .catch(err =>{
+    console.log(err);
+  })
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode=req.query.edit;
+  if(!editMode){
+    return res.redirect('/');
+  }
+  const prodId=req.params.productId;
+  Product.findById(prodId,product=>{
+    if(!product){
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product:product
+    });
+  }); 
+};
+exports.postEditProduct=(req,res,next)=>{
+  const prodId=req.body.productId;
+  const updatedTitle=req.body.title;
+  const updatedPrice=req.body.price;
+  const updatedDesc=req.body.description;
+  const updatedImageUrl=req.body.imageUrl;
+  const updatedProducts=new Product(
+    prodId,updatedTitle,updatedImageUrl,updatedDesc,updatedPrice
+    );
+  updatedProducts.save();
+  res.redirect('/products');
+}
+exports.postDeleteProduct=(req,res,next)=>{
+  const prodId=req.body.productId;
+  Product.deleteProductById(prodId)
+  .then(() => {
+    res.redirect('/products');
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  });
+}
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then(([rows])=>{
+      res.render('admin/products', {
+        prods: rows,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  
+};

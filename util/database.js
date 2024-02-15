6740bf80ddfbf73b351bf249748cc2a7d75efb22
monish17 <monishkumarft@gmail.com>
2024-02-15 15:20:30 +0530

    Complete E-commerce website with Cart functionality

---
+++ util/database.js
@@ -0,0 +1,9 @@
+const Sequelize= require('sequelize');
+
+const sequelize= new Sequelize('node-complete','root','Monish 12345',{
+    dialect:'mysql',
+    host:'localhost'
+});
+
+module.exports= sequelize;
+

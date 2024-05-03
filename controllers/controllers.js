2e2b38beff6fe5e07d3285e63bce7b5560feee68
monish17 <monishkumarft@gmail.com>
2024-05-03 16:49:58 +0530

    Report Generating Backend

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -18,13 +18,14 @@ const S3Services=require('../services/s3Services');
 
 
 function generateAccessToken(id,key){
-  return token.sign({userId:id,isPremiumUser:key},'dune17');
+  const SALT=process.env.SALT;
+  return token.sign({userId:id,isPremiumUser:key},SALT);
 }
 
 function uploadToS3(data,fileName){
-  const BUCKET_NAME='expensetrackingapp1712';
-  const IAM_USER_KEY='AKIAW3MD7JP7EURBO53B';
-  const IAM_USER_SECRET='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';
+  const BUCKET_NAME= process.env.BUCKET_NAME;
+  const IAM_USER_KEY= process.env.IAM_USER_KEY;
+  const IAM_USER_SECRET= process.env.IAM_USER_SECRET;
 
   let s3bucket=new AWS.S3({
     accessKeyId:IAM_USER_KEY,

5ce447975af2ca7e62eb0ba1d062170ce06126be
monish17 <monishkumarft@gmail.com>
2024-05-03 16:56:21 +0530

    Report Generating Backend

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -21,38 +21,6 @@ function generateAccessToken(id,key){
   const SALT=process.env.SALT;
   return token.sign({userId:id,isPremiumUser:key},SALT);
 }
-
-function uploadToS3(data,fileName){
-  const BUCKET_NAME= process.env.BUCKET_NAME;
-  const IAM_USER_KEY= process.env.IAM_USER_KEY;
-  const IAM_USER_SECRET= process.env.IAM_USER_SECRET;
-
-  let s3bucket=new AWS.S3({
-    accessKeyId:IAM_USER_KEY,
-    secretAccessKey:IAM_USER_SECRET
-  })
-
-    var params={
-      Bucket:BUCKET_NAME,
-      Key:fileName,
-      Body:data,
-      ACL:'public-read'
-    }
-    return new Promise((resolve,reject)=>{
-      s3bucket.upload(params,(err,s3respnse)=>{
-        if(err){
-          console.log("Something went wrong",err);
-          reject(err);
-        }else{
-          console.log("success",s3respnse);
-          resolve(s3respnse.Location);
-        }
-      })
-    })
-    
-}
-
-
 exports.SignInData = async (req, res, next) => {
   console.log("request arrived in SignInData");
   // console.log("req.body>>>",req.body);
--- services/s3Services.js
+++ services/s3Services.js
@@ -3,9 +3,9 @@ const AWS=require('aws-sdk');
 const S3URLTable=require('../models/S3URLModel');
 
  exports.uploadToS3=(data,fileName)=>{
-    const BUCKET_NAME='expensetrackingapp1712';
-    const IAM_USER_KEY='AKIAW3MD7JP7EURBO53B';
-    const IAM_USER_SECRET='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';
+  const BUCKET_NAME= process.env.BUCKET_NAME;
+  const IAM_USER_KEY= process.env.IAM_USER_KEY;
+  const IAM_USER_SECRET= process.env.IAM_USER_SECRET;
   
     let s3bucket=new AWS.S3({
       accessKeyId:IAM_USER_KEY,

ceeb49ac5440a51411d26d4fa77f06c8d3e69f54
monish17 <monishkumarft@gmail.com>
2024-08-22 22:31:57 +0530

    .

--- Public/ForgetPassword/ForgetPassword.js
+++ Public/ForgetPassword/ForgetPassword.js
@@ -4,7 +4,7 @@ document.getElementById('sendEmail').onclick= function(e){
     console.log('button is clicked');
     const emailInput=document.getElementById('emailInput');
     console.log(emailInput.value);
-    axios.post(`http://${config.PORT_ADDRESS}:3000/password/forgotpassword`,{"email":emailInput.value}).
+    axios.post(`http://52.172.46.245:3000/password/forgotpassword`,{"email":emailInput.value}).
     then(response =>{
       //console.log(response);
       window.location.href="../SignIn/SignIn.html";
--- Public/SignUp/SignUp.js
+++ Public/SignUp/SignUp.js
@@ -29,7 +29,7 @@ function showMessage(message) {
 }
 
 function signUpData(myobj){
-    axios.post(`http://${config.PORT_ADDRESS}:3000/routes/SignUpData`,myobj)
+    axios.post(`http://52.172.46.245:3000/routes/SignUpData`,myobj)
         .then((response)=>{
             console.log(response);
             if(response.data.message==='Name or Email Id Already registered'||response.data.message==='Internal Server Error'){
